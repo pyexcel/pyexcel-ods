ccf0e63e134583d6ed45fa0c61d2ea9f41220dc6
chfw <wangc_2011@hotmail.com>
2014-12-05 23:29:02 +0000

    travis config

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open("README.rst", 'r') as readme:
     README_txt = readme.read()
 
 dependencies = [
-        'odfpy'
+    'odfpy',
+    'pyexcel-io'
     ]
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     dependencies.append('ordereddict')
