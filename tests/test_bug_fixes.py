d8615b0f465f4906cbb2f25758b47ec8fbcfb53d
chfw <wangc_2011@hotmail.com>
2015-05-28 09:29:47 +0100

    remove print, minor

--- tests/test_bug_fixes.py
+++ tests/test_bug_fixes.py
@@ -4,6 +4,5 @@ from pyexcel_ods import get_data
 
 def test_bug_fix_for_issue_1():
     data = get_data(os.path.join("tests", "fixtures", "repeated.ods"))
-    print data["Sheet1"]
     assert data['Sheet1'] == [['repeated', 'repeated', 'repeated', 'repeated']]
                     
