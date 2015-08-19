6e933320f67017f5fb0c4643641aeeb32c7a45c4
chfw <wangc_2011@hotmail.com>
2015-08-19 22:45:43 +0100

    update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 7:
 setup(
     name='pyexcel-ods',
     author="C. W.",
-    version='0.0.7',
+    version='0.0.9',
     author_email="wangc_2011@hotmail.com",
     url="https://github.com/chfw/pyexcel-ods",
     description='A wrapper library to read, manipulate and write data in ods format',
