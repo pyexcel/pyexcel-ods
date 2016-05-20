a43300a6010d16f8c173f480b58d9da7ec376952
chfw <wangc_2011@hotmail.com>
2016-05-20 18:18:26 +0100

    test with latest odfpy and on all other python platforms

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-cov --cov pyexcel_ods --cov tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_ods
+nosetests --with-cov --cover-package pyexcel_ods --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_ods
