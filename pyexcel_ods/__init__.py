15ace93985d5052c738c565f4f8409bd0d67ed33
chfw <wangc_2011@hotmail.com>
2016-05-23 09:59:48 +0100

    adding missing file

---
+++ pyexcel_ods/__init__.py
@@ -0,0 +1,25 @@
+"""
+    pyexcel_ods
+    ~~~~~~~~~~~~~~~~~~~
+    The lower level ods file format handler using odfpy
+    :copyright: (c) 2015-2016 by Onni Software Ltd & its contributors
+    :license: New BSD License
+"""
+# this line has to be place above all else
+# because of dynamic import
+__pyexcel_io_plugins__ = ['ods']
+
+
+from pyexcel_io.io import get_data as read_data, isstream, store_data as write_data
+
+
+def save_data(afile, data, file_type=None, **keywords):
+    if isstream(afile) and file_type is None:
+        file_type = 'ods'
+    write_data(afile, data, file_type=file_type, **keywords)
+
+
+def get_data(afile, file_type=None, **keywords):
+    if isstream(afile) and file_type is None:
+        file_type = 'ods'
+    return read_data(afile, file_type=file_type, **keywords)
