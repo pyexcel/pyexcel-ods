b28c35fb1a161942b577ce7e22691a7327c39abd
chfw <wangc_2011@hotmail.com>
2016-08-22 17:58:24 +0100

    include flake8 exceptions

---
+++ .moban.d/test.sh
@@ -0,0 +1,5 @@
+{% extends "test.sh.jj2" %}
+
+{%block flake8_options%}
+--builtins=unicode,xrange,long
+{%endblock%}
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 
 pip freeze
-nosetests --with-cov --cover-package pyexcel_ods --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_ods  && flake8 . --exclude=.moban.d 
+nosetests --with-cov --cover-package pyexcel_ods --cover-package tests --with-doctest --doctest-extension=.rst tests README.rst pyexcel_ods  && flake8 . --exclude=.moban.d --builtins=unicode,xrange,long
+
