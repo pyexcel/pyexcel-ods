1827796f19d0f9033e2c119fffad740dc730fa4d
chfw <wangc_2011@hotmail.com>
2019-03-16 14:14:55 +0000

    :egg: :ferris_wheel: release 0.5.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'Onni Software Ltd.'
 # The short X.Y version
 version = '0.5.5'
 # The full version, including alpha/beta/rc tags
-release = '0.5.4'
+release = '0.5.5'
 
 # -- General configuration ---------------------------------------------------
 
