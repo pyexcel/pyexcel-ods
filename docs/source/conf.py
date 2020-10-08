ebb9c2f8f286a4e1d55ae60ed84d097ba0346f5d
chfw <wangc_2011@hotmail.com>
2020-10-08 22:46:46 +0100

    :egg: :ferris_wheel: release 0.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = 'chfw'
 # The short X.Y version
 version = '0.6.0'
 # The full version, including alpha/beta/rc tags
-release = '0.5.6'
+release = '0.6.0'
 
 # -- General configuration ---------------------------------------------------
 
